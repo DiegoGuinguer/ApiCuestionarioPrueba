affefa6c442c22db230888ea8df748ea6458815e
Diego Guinguer <dguinguer@gmail.com>
2024-05-05 16:22:46 -0500

    Feat: Inyeccion de servicio en program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSqlServer<CuestionarioContext>(builder.Configuration.GetConnectionString("cuestionario"));
+builder.Services.AddSqlServer<CuestionarioContext>(builder.Configuration.GetConnectionString("persona"));
+
 builder.Services.AddScoped<ICuestionarioService, CuestionarioService>();
+builder.Services.AddScoped<IPersonaService, PersonaService>();
 
 var app = builder.Build();
 
